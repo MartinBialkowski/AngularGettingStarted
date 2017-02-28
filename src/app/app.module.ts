abecd8fa38d358dce84ac213f0f9b56e92af8649
Bialkowski <martin.bialkowski@corp.altegrity.com>
2017-02-28 09:53:12 +0100

    FormsTutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { HttpModule } from '@angular/http';
 import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
+import { UserComponent } from './FormsTutorial/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    UserComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot([
       {path: `welcome`, component: WelcomeComponent},
+      {path: `user`, component: UserComponent},
       {path: `**`, redirectTo: `welcome`, pathMatch: `full`}
     ]),
     ProductModule
